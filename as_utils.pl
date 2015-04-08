2fe50149334da2cf2bb4eff52518d3706e8229ba
duspeccoll <kevin.clair@du.edu>
2015-04-08 07:54:50 -0600

    updates to as_utils

--- as_utils.pl
+++ as_utils.pl
@@ -11,50 +11,10 @@ use Switch;
 
 my $ua = LWP::UserAgent->new;
 
-# login(url)
-# Handles logging into the ArchivesSpace backend.
-#
-# Params:
-# * url -- the URL to the ArchivesSpace backend.
-
-sub login {
-	my $s;
-	my $n;
-	print "Logging into $_[0]... \n";
-	if($_[1]) { $n = $_[1]; } else { $n = 0; }
-	print "login: ";
-	my $login = <STDIN>;
-	chomp($login);
-	print "password: ";
-	ReadMode 2;
-	my $pwd = <STDIN>;
-	chomp($pwd);
-	ReadMode 0;
-	print "\n";
-
-	my $server_endpoint = "$_[0]/users/$login/login?password=$pwd";
-	my $req = HTTP::Request->new(POST => $server_endpoint);
-	my $resp = $ua->request($req);
-	if($resp->is_success) {
-		my $response = decode_json($resp->decoded_content);
-		$s = $response->{session};
-	} else {
-		$n++;
-		if($n == 5) { 
-			die "Login aborted after five attempts\n";
-		} else {
-			print "Login failed.\n";
-			$s = &login($_[0], $n);
-		}
-	}
-
-	$s;
-}
-
 # get_file()
-# Get the filename. Expects JSON.
+# Gets a file from user input.
 #
-# TODO: Validate upon receipt
+# TODO: Test if the JSON is valid
 
 sub get_file {
 	print "Enter a valid JSON file: ";
@@ -64,8 +24,71 @@ sub get_file {
 		print "$f does not exist.\n";
 		$f = &get_file();
 	}
+	
+	my $json;
+	{
+		local $/;
+		open my $fh, '<', $f or die "Can't open $f: $!\n";
+		$json = <$fh>;
+		close $fh;
+	}
 
-	$f;
+	$json;
+}
+
+# login(url)
+# Handles logging into the ArchivesSpace backend.
+#
+# Params:
+# * url -- the URL to the ArchivesSpace backend.
+# * count -- a variable that checks how many login attempts have been made.
+#     (login() stops trying after five attempts)
+
+sub login {
+	my $url;
+	my $count;
+	my $login;
+	my $password;
+	my $session;
+
+	if($_[0]) { $url = $_[0]; } else {
+		print "URL: ";
+		$url = <STDIN>;
+		chomp($url);
+	}
+	print "Logging into $url... \n";
+	if($_[1]) { $count = $_[1]; } else { $count = 0; }
+	if($_[2]) { $login = $_[2]; } else {
+		print "login: ";
+		$login = <STDIN>;
+		chomp($login);
+	}
+	if($_[3]) { $password = $_[3]; } else {
+		print "password: ";
+		ReadMode 2;
+		$password = <STDIN>;
+		chomp($password);
+		ReadMode 0;
+	}
+	print "\n";
+
+	my $server_endpoint = "$url/users/$login/login?password=$password";
+	my $req = HTTP::Request->new(POST => $server_endpoint);
+	my $resp = $ua->request($req);
+	if($resp->is_success) {
+		my $response = decode_json($resp->decoded_content);
+		$session = $response->{session};
+	} else {
+		$count++;
+		if($count == 5) { 
+			die "Login aborted after five attempts\n";
+		} else {
+			print "Login failed.\n";
+			$session = &login($url, $count);
+		}
+	}
+
+	$session;
 }
 
 # get_agent_class(path)
